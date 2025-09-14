07675a682664ce6e25ea63a65dbc2b21c98bf428
Paul Emmanuel Chukwuma <chukspaul777@gmail.com>
2025-09-14 20:42:18 +0100

    concat

---
+++ 4-concat.js
@@ -0,0 +1,7 @@
+#!/usr/bin/node
+
+// Access command-line arguments from process.argv
+const firstArg = process.argv[2];
+const secondArg = process.argv[3];
+
+console.log(`${firstArg} is ${secondArg}`);
